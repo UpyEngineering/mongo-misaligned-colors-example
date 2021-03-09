82bc8fbaf5ef14d52421fa17396db0e6cd5bb5d4
jpiraguha <jiraguha@gmail.com>
2021-03-09 17:38:20 +0100

    chore

--- src/test/kotlin/io/joinupy/example/mongomisalignedcolorsexample/service/ColorServiceTest.kt
+++ src/test/kotlin/io/joinupy/example/mongomisalignedcolorsexample/service/ColorServiceTest.kt
@@ -1,13 +1,11 @@
 package io.joinupy.example.mongomisalignedcolorsexample.service
 
-import brave.Tracing
 import io.joinupy.example.mongomisalignedcolorsexample.ColorTest
 import io.joinupy.example.mongomisalignedcolorsexample.config.StartupDataConfig
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import reactor.kotlin.test.test
 
 @ColorTest
